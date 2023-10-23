c46739060ad43e89835e345672cc747f6c75c04a
knownaszaid <59281993+SmartZaidu@users.noreply.github.com>
2023-10-23 13:06:03 +0530

    Add files via upload

---
+++ January.java
@@ -0,0 +1,25 @@
+class January
+{
+	public static void main(String []args)
+		{
+			System.out.println("Arithmetic operations");
+			int a=20;
+			int b=10;
+			//int c=15;
+			int sum = a+b;
+			int sub = a-b;
+			int mul = a*b;
+			int div = a/b;
+			int modulus=a%b;
+			//s= a//b;
+			System.out.println(sum+" \n"+sub+" \n"+mul+" \n"+div+" \n"+modulus);
+			System.out.println(a>b);
+			System.out.println(a<b);
+			System.out.println(a>=b);
+			System.out.println(a>=b);
+			System.out.println(a!=b);
+			System.out.println(a+=b);
+			System.out.println(a-=b);					
+			
+}
+}
